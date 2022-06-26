e03c3d3a3e75e774ac7fc1c69f79c4b17879b596
Akshar Vandara <axr230102@gmail.com>
2022-06-26 12:58:29 +0530

    Created Database

--- booklisting/main/models.py
+++ booklisting/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class Book_List(models.Model):
+    Db_Name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Db_Name
+
+
+class Book_Item(models.Model):
+    key = models.ForeignKey(Book_List, on_delete=models.CASCADE)
+    booktitle = models.CharField(max_length=500)
+    cart_boolean = models.BooleanField()
+
+    def __str__(self):
+        return self.booktitle
