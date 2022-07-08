ad6719800e75b5487aef88b8283408f8edb716cc
Akshar Vandara <axr230102@gmail.com>
2022-07-08 16:30:57 +0530

    Final Project done

--- booklisting/main/urls.py
+++ booklisting/main/urls.py
@@ -6,10 +6,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.landingpage, name="landingpage"),
     path("cart/", views.cart, name="cart"),
-    # path("addedtocart", views.addedtocart, name="addedtocart"),
     path("cart/cart_update", views.cart_update, name="cart_update"),
     path("search/",views.InfoListView.as_view(), name="search"),
-    path('', views.index, name ='index'),
+    path('signup/',views.signup, name="signup"),
+    path('signin/',views.signin, name="signin"),
+    path('signout/',views.signout, name="signout"),
+    path("signin/home",views.home, name="home"),
 ]
--- booklisting/main/views.py
+++ booklisting/main/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Book_Item, Book_List
 from django.views.generic import ListView
 import json
+from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
-from django.core.mail import send_mail
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import get_template
-from django.template import Context
-from .forms import UserRegisterForm
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
-
+a = { }
+b = {}
+l = []
 class InfoListView(ListView):
     model = Book_Item
     template_name = 'main/search.html'
@@ -24,30 +18,119 @@ class InfoListView(ListView):
         context["qs_json"] = json.dumps(list(Book_Item.objects.values()))
         return context
 
+
+def clicked(item):
+    item.cart_boolean = True
+    item.count = 1
+    return item.save()
+
+def landingpage(response):
+    bl = Book_List.objects.get(id=1)
+    return render(response, 'main/landingpage.html', {"bl":bl})
+
 def home(response):
     bl = Book_List.objects.get(id=1)
     bi = Book_Item.objects.all()
-    for item in bi:
-
-        if response.POST.get('c' + str(item.id)) == "clicked":
-            item.cart_boolean = True
-            item.count = 1
+    user = response.user.get_username()
+    fname = user
+    if user not in a.keys():
+        l1 = []
+        dic = {}
+        for item in bi:
+            item.count = 0
+            item.cart_boolean = False
             item.save()
+            dic[item.id] = item.count
+            
+            l1.append(dic)
+            a[user] = l1[0]
 
-        if response.POST.get('m' + str(item.id)) == 'minus':
-            if item.count > 1:
-                item.count -= 1
+            if response.POST.get('c' + str(item.id)) == "clicked":
+                item.cart_boolean = True
+                a[user][item.id] = 1
+                item.count = a[user][item.id]
                 item.save()
-            elif item.count == 1:
-                item.cart_boolean = False
+
+            if response.POST.get('m' + str(item.id)) == 'minus':
+                if item.count > 1:
+                    a[user][item.id] -= 1
+                    item.count = a[user][item.id]
+                    item.save()
+                    # b[item.id] = item.count
+
+
+                elif item.count == 1:
+                    item.cart_boolean = False
+                    a[user][item.id] = 0
+                    item.save()
+
+                    # b.pop(item.id)
+
+                # a[user] = l
+
+
+            elif response.POST.get('p' + str(item.id)) == 'plus':
+                a[user][item.id] += 1
+                item.count = a[user][item.id]
                 item.save()
+    
+    else:
+        # for i in len(a[user][0]):
+        for item in bi:
+            # if user in a.keys():
+            # l1 = []
+            # a[user] = l1.append({item.id : item.count})
+            if response.POST.get('c' + str(item.id)) == "clicked":
 
-        elif response.POST.get('p' + str(item.id)) == 'plus':
-            item.count += 1
-            item.save()
+                item.cart_boolean = True
+                a[user][item.id] = 1
+                item.count = a[user][item.id]
+                item.save()
+                # l.append(item.id)
+                # l.append(item.count)
+                # a[user] = l
+                # print(a)
 
-    return render(response, 'main/home.html', {"bl":bl})
+                # if item.id not in b.keys():
+                #     b[item.id] = item.count
 
+                # else:
+                #     pass
+
+                # a[user] = l
+
+
+            if response.POST.get('m' + str(item.id)) == 'minus':
+                if item.count > 1:
+                    a[user][item.id] -= 1
+                    item.count = a[user][item.id]
+                    item.save()
+                    # b[item.id] = item.count
+
+
+                elif item.count == 1:
+                    item.cart_boolean = False
+                    a[user][item.id] = 0
+                    item.save()
+
+                    # b.pop(item.id)
+
+                # a[user] = l
+
+
+            elif response.POST.get('p' + str(item.id)) == 'plus':
+                a[user][item.id] += 1
+                item.count = a[user][item.id]
+                item.save()
+                # b[item.id] = item.count
+                # a[user] = l
+
+
+
+    print(a)
+    return render(response, 'main/home.html', {"bl":bl, 'fname':fname})
+
+# l.append(b)
 
 def cart(response):
     bl = Book_List.objects.get(id=1)
@@ -57,55 +140,82 @@ def cart(response):
 def cart_update(response):
     bl = Book_List.objects.get(id=1)
     bi = Book_Item.objects.all()
+    user = response.user.get_username()
+    fname = user
     for item in bi:
         
         if response.POST.get('m' + str(item.id)) == 'minus':
             if item.count > 1:
-                item.count -=1
+                a[user][item.id] -= 1
+                item.count = a[user][item.id]
                 item.save()
+                # b[item.id] = item.count
             elif item.count == 1:
                 item.cart_boolean = False
+                a[user][item.id] = 0
                 item.save()
+                # b.pop(item.id)
+            # a[user] = l
+
 
         elif response.POST.get('p' + str(item.id)) == 'plus':
-            item.count +=1
+            a[user][item.id] += 1
+            item.count = a[user][item.id]
             item.save()
-        
+            
+    print(a)    
     return render(response, 'main/cart.html', {"bl":bl})
 
-def index(request):
-    return render(request, 'main/index.html', {'title':'index'})
+def signup(request):
 
-def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
-            htmly = get_template('main/Email.html')
-            d = { 'username': username }
-            subject, from_email, to = 'welcome', 'booklistingtest@gmail.com', email
-            html_content = htmly.render(d)
-            msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
-            msg.attach_alternative(html_content, "text/html")
-            msg.send()
-            messages.success(request, f'Your account has been created! You can log in now!')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'main/register.html', {'form': form, 'title':'Register Here'})
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
 
-def Login(request):
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been created.")
+
+        return redirect('signin')
+
+    return render(request, "main/signup.html")
+
+def signin(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username = username, password = password)
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass1')
+
+        user = authenticate(username=username, password=pass1)
+
         if user is not None:
-            form = login(request, user)
-            messages.success(request, f' wecome {username} !!')
-            return redirect('index')
+            login(request, user)
+            fname = user.first_name
+            bl = Book_List.objects.get(id=1)
+            context = {'fname':fname,"bl":bl}
+            return render(request, "main/home.html", {'fname':user,"bl":bl})
+
+
         else:
-            messages.info(request, f'Username or password is incorrect. Please log in with correct credentials or create a new account.')
-    form = AuthenticationForm()
-    return render(request, 'main/login.html', {'form':form, 'title':'log in'})
+            messages.error(request, "Bad credentials")
+            return redirect('signin')
+
+    return render(request, "main/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged out Successfully!")
+    return redirect('signin')
+
+
+#
+# {'user':[item_id, item_count]}
+# {'heta': [15,2]
+#  'aum':[16,1]}
