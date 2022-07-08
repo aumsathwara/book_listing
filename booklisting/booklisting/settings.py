da4f6e709be6d24a447a09d8d44aa742ee17ebfa
Heta Chokshi <63964379+hetachokshi@users.noreply.github.com>
2022-07-08 18:05:23 +0530

    settings.py edit done

--- booklisting/booklisting/settings.py
+++ booklisting/booklisting/settings.py
@@ -132,8 +132,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "booklistingtest@gmail.com"
-EMAIL_HOST_PASSWORD = "uudxxvnteakborjm"
+EMAIL_HOST_USER = "Enter Host Email"
+EMAIL_HOST_PASSWORD = "Enter Host Password"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
